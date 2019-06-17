87b7740af7524487ed6797e48a8c0601f3d76e81
shashank <47443963+guptashak90@users.noreply.github.com>
2019-06-17 13:36:38 +0530

    Create even_odd.js

---
+++ even_odd.js
@@ -0,0 +1,22 @@
+function evenOddCheck(number) {
+    var num = number;
+    var num = JSON.stringify(num);
+    var evenSum = 0, oddSum = 0, value;
+    var tempArr = num.split('');
+    for (var i = 0; i < num.length; i++) {
+        if (i % 2 === 0) {
+            value = JSON.parse(tempArr[i])
+            evenSum =+ value;
+        } else {
+            ovalue = JSON.parse(tempArr[i])
+            oddSum =+ value;
+        }
+    }
+    if (evenSum == oddSum) {
+        console.log(true)
+    } else {
+        console.log(false);
+    }
+}
+
+evenOddCheck(121);
